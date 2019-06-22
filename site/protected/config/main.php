f71f9404b51a489f1ce361bd402b45207d414798
George <georgyfunds@gmail.com>
2019-06-22 14:56:46 +0600

    Название

--- site/protected/config/main.php
+++ site/protected/config/main.php
@@ -9,6 +9,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Обучающий портал',
 	'theme'=>'bootstrap',
+    'language' => 'ru',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
